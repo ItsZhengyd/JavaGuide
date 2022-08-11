3bc67b9f8fbc0fdd81de13e5550b161eb4c21e7e
zhengyd <itszhengyadong@qq.com>
2022-08-11 17:15:23 +0800

    分离SmallTool

--- thread/src/test/java/CompletableFutureTest.java
+++ thread/src/test/java/CompletableFutureTest.java
@@ -204,24 +204,4 @@ public class CompletableFutureTest {
         System.out.println("result = " + result);
     }
 
-    static class SmallTool {
-        public static void sleepMillis(int millis) {
-            try {
-                TimeUnit.MILLISECONDS.sleep(millis);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static void printTimeAndThread(String tag) {
-            String result = new StringJoiner("\t|\t")
-                    .add(String.valueOf(System.currentTimeMillis()))
-                    .add(String.valueOf(Thread.currentThread().getId()))
-                    .add(Thread.currentThread().getName())
-                    .add(tag)
-                    .toString();
-            System.out.println(result);
-        }
-    }
-
 }
