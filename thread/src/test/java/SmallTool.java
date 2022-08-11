964808b90334daf493c711dadda235a572fdd426
zhengyd <itszhengyadong@qq.com>
2022-08-11 17:15:52 +0800

    分离SmallTool

---
+++ thread/src/test/java/SmallTool.java
@@ -0,0 +1,22 @@
+import java.util.StringJoiner;
+import java.util.concurrent.TimeUnit;
+
+public class SmallTool {
+    public static void sleepMillis(int millis) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void printTimeAndThread(String tag) {
+        String result = new StringJoiner("\t|\t")
+                .add(String.valueOf(System.currentTimeMillis()))
+                .add(String.valueOf(Thread.currentThread().getId()))
+                .add(Thread.currentThread().getName())
+                .add(tag)
+                .toString();
+        System.out.println(result);
+    }
+}
