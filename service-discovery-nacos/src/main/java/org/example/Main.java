7584742b6e5cf2b616f0fc9b4e8413e3ab20dc03
zhengyd <itszhengyadong@qq.com>
2022-08-12 23:48:14 +0800

    nacos

---
+++ service-discovery-nacos/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a href="https://github.com/alibaba/spring-cloud-alibaba/wiki/%E7%89%88%E6%9C%AC%E8%AF%B4%E6%98%8E">spring-cloud-alibaba毕业版本依赖关系</a>
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
